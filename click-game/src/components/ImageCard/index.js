cc249989b9dca8f6c0b4f08e0df2553d5fe8fbbb
sylvo <49357334+sjonon@users.noreply.github.com>
2019-07-30 23:43:44 -0500

    added styling and shuffle function

--- click-game/src/components/ImageCard/index.js
+++ click-game/src/components/ImageCard/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function ImageCard(props){
     return (
         <div className="image-container">
-            <img className="z-depth-3 waves-effect waves-teal" onClick={()=>props.imageClick(props.id)} src={props.imageUrl} alt={props.name}/>
+            <img className="z-depth-3" id={props.animate} onClick={()=>props.imageClick(props.id)} src={props.imageUrl} alt={props.name}/>
         </div>
     )
 }
