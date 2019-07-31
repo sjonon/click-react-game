9e4ffcaf813941e21e8736daa1205db78b3e328f
sylvo <49357334+sjonon@users.noreply.github.com>
2019-07-31 00:04:13 -0500

    tweaked to modify width of instruction container

---
+++ click-game/build/precache-manifest.1ac98ae99a979c9bbb4297fe23206f33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93a5a908197be45adff39ee48c4c0d1",
+    "url": "/click-react-game/index.html"
+  },
+  {
+    "revision": "ec63df93f6e9062f8625",
+    "url": "/click-react-game/static/css/main.c0251fed.chunk.css"
+  },
+  {
+    "revision": "c6bd0d203e320d08a191",
+    "url": "/click-react-game/static/js/2.58b98f68.chunk.js"
+  },
+  {
+    "revision": "ec63df93f6e9062f8625",
+    "url": "/click-react-game/static/js/main.01c6c387.chunk.js"
+  },
+  {
+    "revision": "5e3843503923a6367a95",
+    "url": "/click-react-game/static/js/runtime~main.c4180d67.js"
+  }
+]);
